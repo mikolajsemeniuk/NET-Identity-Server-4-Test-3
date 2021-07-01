f94b781060704edfd5374c65c121f5e50ba24b56
Mikolaj Semeniuk <m.semeniuk@icloud.com>
2021-07-01 23:58:32 +0200

    correct names

--- source/app/services/client.service/Controllers/AuthController.cs
+++ source/app/services/client.service/Controllers/AuthController.cs
@@ -18,7 +18,7 @@ namespace client.service.Controllers
         [HttpGet]
         public async Task<ActionResult> Get()
         {
-            var apiClientCredentials = new ClientCredentialsTokenRequest
+            var credentials = new ClientCredentialsTokenRequest
             {
                 Address = "https://localhost:5001/connect/token",
                 // GrantType = "client_credentials",
@@ -31,22 +31,19 @@ namespace client.service.Controllers
 
             var disco = await client.GetDiscoveryDocumentAsync("https://localhost:5001");
             if (disco.IsError)
-            {
                 return null;
-            }
 
-            var token = await client.RequestClientCredentialsTokenAsync(apiClientCredentials);
+            var token = await client.RequestClientCredentialsTokenAsync(credentials);
             if (token.IsError)
-            {
-                
-            }
+                return null;
 
             var apiClient = new HttpClient();
             apiClient.SetBearerToken(token.AccessToken);
+            
             var response = await apiClient.GetAsync("https://localhost:6001/api/customer");
             response.EnsureSuccessStatusCode();
+            
             var content = await response.Content.ReadAsStringAsync();
-
             return Ok(content);
         }
     }
